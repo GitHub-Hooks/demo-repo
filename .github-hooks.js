330866136cf628baa685c6eb960c0844de539424
ewolfe <e@ewolfe.me>
2017-11-20 13:20:45 -0800

    Modify the pull request title

--- .github-hooks.js
+++ .github-hooks.js
@@ -1,5 +1,13 @@
 const hooks = {
   'compare-&-pull-request': (meta) => {
     window.alert(meta.branch)
+    if (/^(feat|feature|fix|docs|style|refactor|perf|test|chore)\(\w+\)?\//.test(meta.branch)) {
+      const branchName = meta.branch.split('/')
+      const pullRequestTitle = `${branchName[0]}: ${branchName[1].replace('-', ' ')}`;
+      const pullRequestTitleElement = document.getElementById('pull_request_title');
+      pullRequestTitleElement.value = pullRequestTitle;
+    } else {
+      window.alert('Please use the proper branch naming schema');
+    }
   }
 }
