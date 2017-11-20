13ec592378ad381e9549c365c64305ad6eec4a06
ewolfe <e@ewolfe.me>
2017-11-20 13:28:03 -0800

    Replace all dashes (instead of just the first one)

--- .github-hooks.js
+++ .github-hooks.js
@@ -3,7 +3,7 @@ const hooks = {
     window.alert(meta.branch)
     if (/^(feat|feature|fix|docs|style|refactor|perf|test|chore)(\(\w+\))?\//.test(meta.branch)) {
       const branchName = meta.branch.split('/')
-      const pullRequestTitle = `${branchName[0]}: ${branchName[1].replace('-', ' ')}`;
+      const pullRequestTitle = `${branchName[0]}: ${branchName[1].replace(/-/g, ' ')}`;
       const pullRequestTitleElement = document.getElementById('pull_request_title');
       pullRequestTitleElement.value = pullRequestTitle;
     } else {
