5dba4fde6982feb3ce97e94d2f16fb2b61d1cf90
ewolfe <e@ewolfe.me>
2017-11-20 13:25:47 -0800

    Fix regular expression

--- .github-hooks.js
+++ .github-hooks.js
@@ -1,7 +1,7 @@
 const hooks = {
   'compare-&-pull-request': (meta) => {
     window.alert(meta.branch)
-    if (/^(feat|feature|fix|docs|style|refactor|perf|test|chore)\(\w+\)?\//.test(meta.branch)) {
+    if (/^(feat|feature|fix|docs|style|refactor|perf|test|chore)(\(\w+\))?\//.test(meta.branch)) {
       const branchName = meta.branch.split('/')
       const pullRequestTitle = `${branchName[0]}: ${branchName[1].replace('-', ' ')}`;
       const pullRequestTitleElement = document.getElementById('pull_request_title');
