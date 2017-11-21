c0f3573d58ea2226290dce2c3c44f3008c40c969
ewolfe <e@ewolfe.me>
2017-11-20 17:46:25 -0800

    Test access to 'chrome' object

--- .github-hooks.js
+++ .github-hooks.js
@@ -1,6 +1,5 @@
 const hooks = {
   'compare-&-pull-request': (meta) => {
-    window.alert(meta.branch)
     if (/^(feat|feature|fix|docs|style|refactor|perf|test|chore)(\(\w+\))?\//.test(meta.branch)) {
       const branchName = meta.branch.split('/')
       const pullRequestTitle = `${branchName[0]}: ${branchName[1].replace(/-/g, ' ')}`;
@@ -9,5 +8,14 @@ const hooks = {
     } else {
       window.alert('Please use the proper branch naming schema');
     }
+
+    chrome.storage.sync.get({
+      token: null,
+    }, function({ token }) {
+      console.log(token);
+      chrome.tabs.create({
+        url: `https://example.com`
+      })
+    });
   }
 }
