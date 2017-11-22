0c0024ae63ce3fb99a6ef3a5e62453240a622153
ewolfe <e@ewolfe.me>
2017-11-22 15:37:32 -0800

    Simplify demo

--- .github-ui-hooks.js
+++ .github-ui-hooks.js
@@ -1,12 +1,6 @@
 const GITHUB_UI_HOOKS = {
   'pull-request-compare': (meta) => {
-    if (/^(feat|feature|fix|docs|style|refactor|perf|test|chore)(\(\w+\))?\//.test(meta.branch)) {
-      const branchName = meta.branch.split('/')
-      const pullRequestTitle = `${branchName[0]}: ${branchName[1].replace(/-/g, ' ')}`;
-      const pullRequestTitleElement = document.getElementById('pull_request_title');
-      pullRequestTitleElement.value = pullRequestTitle;
-    } else {
-      window.alert('Please use the proper branch naming schema');
-    }
+    const title = document.getElementById('pull_request_title');
+    title.value = 'Hello world';
   }
 }
